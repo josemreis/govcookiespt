d01b345b8edd7cc8e919c1bfd0fd7552a3ec35f3
José Reis <josemreis@pm.me>
2022-07-29 08:30:48 +0100

    write a bash script for running vpnless single country audits with retry

--- scripts/run-single-country-trial.sh
+++ scripts/run-single-country-trial.sh
@@ -6,7 +6,7 @@ echo "[!] This trial will not use any vpn"
 audit_n=0
 for attempt in {1..10}; do
     let "audit_n+=1"
-    bash scripts/run-audits.sh --replications 10 --location "pt" --name-prefix "audit_novpn_${audit_n}"
+    bash scripts/run-audits.sh --replications 10 --location "pt" --name-prefix "audit_novpn_${audit_n}" -headless
     ret=$?
     if [ $ret -eq 0 ]; then
         break
